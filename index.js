a6e8b16c31c398bbc5fadccc575314df259f801e
k3tanyadav <ketany798@gmail.com>
2023-05-13 02:06:32 +0530

    first commit :setting up directory structure and express server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const port = 8000;
+const app = express();
+
+app.listen(port, function(err){
+    if(err){
+        console.log(`ERROR : ${err}`);
+        return;
+    }
+    console.log(`server running on port : ${port}`);
+})
