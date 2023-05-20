af17d2c035903f1600feb94f69063c3949d042c7
k3tanyadav <ketany798@gmail.com>
2023-05-21 00:13:23 +0530

    add functionality to switch b/w light and dark theme

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,6 +1,8 @@
-//listen for value change on task checkbox
 let taskContainer = document.getElementById('task-container');
+let toggler = document.getElementById('toggle');
+let body = document.getElementsByTagName('body')[0];
 
+//listen for value change on task checkbox
 taskContainer.addEventListener('click', (e)=>{
     if(e.target.type === "checkbox"){
         let taskId = e.target.id;
@@ -9,4 +11,20 @@ taskContainer.addEventListener('click', (e)=>{
 
         window.location.href = `/modify-task/?id=${taskId}&toggle=${value}`;
     }
+})
+
+//listen for app theme toggle
+toggler.addEventListener('click',(e)=>{
+    let value = e.target.classList[1];
+    if(value === 'fa-toggle-on'){
+        e.target.classList.add('fa-toggle-off');
+        body.style.backgroundColor = "lavender";
+        body.style.color = "black";
+    }
+    else{
+        e.target.classList.add('fa-toggle-on');
+        body.style.backgroundColor = "#635569";
+        body.style.color = "beige";
+    }
+    e.target.classList.remove(value);
 })
