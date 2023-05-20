ca3173d6dfaa145dca8aac2f16f8ad175c5181dc
k3tanyadav <ketany798@gmail.com>
2023-05-21 03:17:15 +0530

    retain theme on page reload

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,5 +1,5 @@
 let taskContainer = document.getElementById('task-container');
-let toggler = document.getElementById('toggle');
+let theme = document.querySelector('#toggle i');
 let body = document.getElementsByTagName('body')[0];
 
 //listen for value change on task checkbox
@@ -9,22 +9,23 @@ taskContainer.addEventListener('click', (e)=>{
         let value = e.target.checked;
         console.log(taskId, value);
 
+        //task update handled by modify task in home_controller file
         window.location.href = `/modify-task/?id=${taskId}&toggle=${value}`;
     }
 })
 
 //listen for app theme toggle
-toggler.addEventListener('click',(e)=>{
-    let value = e.target.classList[1];
-    if(value === 'fa-toggle-on'){
-        e.target.classList.add('fa-toggle-off');
-        body.style.backgroundColor = "lavender";
-        body.style.color = "black";
+theme.addEventListener('click',(e)=>{
+    body.classList.toggle('dark');
+    theme.classList.toggle('fa-toggle-on');
+    localStorage.setItem("toggle",theme.classList[2]);
+})
+
+// retain theme when the page reloads due to task deletion or updation using local storage
+window.addEventListener('DOMContentLoaded', (e)=>{
+    let value = localStorage.getItem("toggle");
+    if(value != "undefined"){
+        body.classList.toggle('dark');
+        theme.classList.toggle('fa-toggle-on');
     }
-    else{
-        e.target.classList.add('fa-toggle-on');
-        body.style.backgroundColor = "#635569";
-        body.style.color = "beige";
-    }
-    e.target.classList.remove(value);
 })
